bcf9ab5d7ef304b85ba90103754b6b7973dee104
mazarul islam <complex.logik@gmail.com>
2017-11-05 02:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  if (string === lowercase){
+    return "I can't hear you!"
+  }else if (string === uppercase)
+{
+  return "YES INDEED!"
+}  else if (string === "I love you, Grandma."){
+  return "I love you, too."
+}
+ }
